a99fee742609d03274deee4372dd56d1fee9f476
Karimzade_8222 <karimzademustafa@gmail.com>
2026-02-14 22:49:17 +0500

    Test 2

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let uname = "Ilya"
 alert(`hello ${1}`);
 alert(`hello ${"uname"}`);
 alert(`hello ${uname}`);
+
+let age = prompt('How old are you');
+alert(`Тебе ${age} лет!`);
+
+let Boss = confirm("Are you Boss?");
+alert(Boss);
