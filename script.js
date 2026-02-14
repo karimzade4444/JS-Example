111b87b0201692046066aab5a6faf8ee6967d766
Karimzade_8222 <karimzademustafa@gmail.com>
2026-02-14 22:23:17 +0500

    JS Lesson Test

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let username = "Jhone";
+let admin;
+admin = username;
+alert(admin +" " + username);
+
+let uname = "Ilya"
+alert(`hello ${1}`);
+alert(`hello ${"uname"}`);
+alert(`hello ${uname}`);
