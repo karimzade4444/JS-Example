11a68a2f2ece31369924dc9acb61c2e7f3a50793
Karimzade_8222 <karimzademustafa@gmail.com>
2026-02-15 23:22:14 +0500

    summ

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 "use strict"
 
-let username = "Jhone";
-let admin;
-admin = username;
-alert(admin +" " + username);
-
-let uname = "Ilya"
-alert(`hello ${1}`);
-alert(`hello ${"uname"}`);
-alert(`hello ${uname}`);
-
-let age = prompt('How old are you');
-alert(`Тебе ${age} лет!`);
-
-let Boss = confirm("Are you Boss?");
-alert(Boss);
+let a=1, b=1;
+let c=++a;
+alert(a);
+alert(b);
+alert(c);
