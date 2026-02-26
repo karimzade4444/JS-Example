c080c41723c3efd60f43381434ab793425ab62c4
Karimzade_8222 <karimzademustafa@gmail.com>
2026-02-26 21:54:47 +0500

    Task map 4

--- script.js
+++ script.js
@@ -405,3 +405,14 @@ let range = {
 // };
 
 // obj.name = "Muhammad";
+
+const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [3, 4, 5, 6, 7];
+// const set2 = new Set(arr2)
+// let arr = []
+// arr1.forEach((val)=>{
+// if (set2.has(val)){
+// arr.push(val)
+// }
+// })
+// console.log(arr)
