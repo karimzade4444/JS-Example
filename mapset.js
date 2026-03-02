a8094e376d5d7865a7ebf137f80a14df76aa9a17
Karimzade_8222 <karimzademustafa@gmail.com>
2026-03-02 17:47:33 +0500

    Task map

--- mapset.js
+++ mapset.js
@@ -2,3 +2,15 @@
 // let arr = Array.from(str)
 // let s = new Set(arr)
 // console.log(s.size)
+
+
+// let numbers = [1, 2, 3, 2, 1, 4, 5, 3, 1];
+// let map = new Map();
+// for(let num of numbers){
+//     if (map.has(num)) {
+//     map.set(num, map.get(num) + 1);
+// } else {
+//     map.set(num, 1);
+// }
+// }
+// console.log(map);
