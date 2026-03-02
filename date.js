33bbe7c0a49eb436daf9787e2b1b30dd84b905dc
Karimzade_8222 <karimzademustafa@gmail.com>
2026-03-02 19:20:57 +0500

    Date 2

--- date.js
+++ date.js
@@ -1,2 +1,19 @@
 // let now = new Date();
 // console.log(now)
+
+
+// let now = new Date();
+
+// // Получаем части
+// let day = now.getDate();        // число месяца
+// let month = now.getMonth() + 1; // месяц, +1 потому что 0 = январь
+// let year = now.getFullYear();
+
+// let hours = now.getHours();
+// let minutes = now.getMinutes();
+// let seconds = now.getSeconds();
+
+// // Собираем в строку
+// let formatted = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+// console.log(formatted);
